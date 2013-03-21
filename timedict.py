453901d2ddefacb63c6c42bf60262212cdc50e79
James Fairbanks <james.fairbanks@gatech.edu>
2013-03-21 16:13:28 -0400

    add decorators and improve timedict class

--- timedict.py
+++ timedict.py
@@ -1,4 +1,50 @@
+"""The timedict module provides decorators to wrap your functions for timing counting and logging 
+as well as the timedict class to handle it using the interface of tic and toc.
+"""
 from time import time as time
+from functools import wraps 
+def benchmark(func):
+    """
+    A decorator that prints the time a function takes
+    to execute.
+    """
+    import time
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t = time.clock()
+        res = func(*args, **kwargs)
+        print func.__name__, time.clock()-t
+        return res
+    return wrapper
+
+
+def logging(func):
+    """
+    A decorator that logs the activity of the script.
+    (it actually just prints it, but it could be logging!)
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        print func.__name__, args, kwargs
+        return res
+    return wrapper
+
+
+def counter(func):
+    """
+    A decorator that counts and prints the number of times a function has been executed
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.count = wrapper.count + 1
+        res = func(*args, **kwargs)
+        print "{0} has been used: {1}x".format(func.__name__, wrapper.count)
+        return res
+    wrapper.count = 0
+    return wrapper
+
+
 
 class timedict(object):
     """ The global state necessary to do sophisticated timing
@@ -11,6 +57,7 @@ class timedict(object):
 
     def __init__(self, ):
         """
+        Make some empty dicts
         """
         self.starts = dict()
         self.ends = dict()
@@ -38,3 +85,30 @@ class timedict(object):
 
     def as_dict(self):
         return self.ends
+
+    def __getitem__(self, key):
+        """Access the time for doing key
+
+        :key: The task of interest using the same key as tic and toc
+        :returns: The time it took
+
+        """
+        return self.ends[key]
+
+
+def test():
+    """Just shows of the decorators in this module
+    :returns: None
+
+    """
+    @counter
+    @benchmark
+    @logging
+    def reverse_string(string):
+        return str(reversed(string))
+
+    print reverse_string("Able was I ere I saw Elba")
+    print reverse_string("A man, a plan, a canoe, pasta, heros, rajahs, a coloratura, maps, snipe, percale, macaroni, a gag, a banana bag, a tan, a tag, a banana bag again (or a camel), a crepe, pins, Spam, a rut, a Rolo, cash, a jar, sore hats, a peon, a canal: Panama!")
+
+if __name__ == '__main__':
+    test()
