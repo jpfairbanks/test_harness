a7f203ca2b6f6bcf476749d4b1e4226ff7322399
James Fairbanks <james.fairbanks@gatech.edu>
2013-03-25 07:32:28 -0400

    use parens for py3k

--- timedict.py
+++ timedict.py
@@ -13,7 +13,7 @@ def benchmark(func):
     def wrapper(*args, **kwargs):
         t = time.clock()
         res = func(*args, **kwargs)
-        print func.__name__, time.clock()-t
+        print(func.__name__, time.clock()-t)
         return res
     return wrapper
 
@@ -26,7 +26,7 @@ def logging(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         res = func(*args, **kwargs)
-        print func.__name__, args, kwargs
+        print(func.__name__, args, kwargs)
         return res
     return wrapper
 
@@ -39,7 +39,7 @@ def counter(func):
     def wrapper(*args, **kwargs):
         wrapper.count = wrapper.count + 1
         res = func(*args, **kwargs)
-        print "{0} has been used: {1}x".format(func.__name__, wrapper.count)
+        print("{0} has been used: {1}x".format(func.__name__, wrapper.count))
         return res
     wrapper.count = 0
     return wrapper
@@ -107,8 +107,8 @@ def test():
     def reverse_string(string):
         return str(reversed(string))
 
-    print reverse_string("Able was I ere I saw Elba")
-    print reverse_string("A man, a plan, a canoe, pasta, heros, rajahs, a coloratura, maps, snipe, percale, macaroni, a gag, a banana bag, a tan, a tag, a banana bag again (or a camel), a crepe, pins, Spam, a rut, a Rolo, cash, a jar, sore hats, a peon, a canal: Panama!")
+    print(reverse_string("Able was I ere I saw Elba"))
+    print(reverse_string("A man, a plan, a canoe, pasta, heros, rajahs, a coloratura, maps, snipe, percale, macaroni, a gag, a banana bag, a tan, a tag, a banana bag again (or a camel), a crepe, pins, Spam, a rut, a Rolo, cash, a jar, sore hats, a peon, a canal: Panama!"))
 
 if __name__ == '__main__':
     test()
